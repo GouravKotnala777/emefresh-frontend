08f0b5773ac8912879ac11368cd9386cb989c4d0
gouravkotnala <gouravkotnala777@gmail.com>
2026-03-17 16:44:12 +0530

    feat(App.tsx) : add 'all_products' and 'update_product' routes and ScrollToTop component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, useLocation} from "react-router-dom";
 import Home from './pages/Home.page.tsx';
 import Header from "./components/Header.component.tsx";
 import { useEffect, useState } from "react";
@@ -9,7 +9,19 @@ import { type UserTypes } from "./apis/userApi.ts";
 import CreateProduct from "./pages/CreateProduct.page.tsx";
 import SingleProduct from "./pages/SingleProduct.tsx";
 import Cart from "./pages/Cart.page.tsx";
+import AllProducts from "./pages/AllProducts.page.tsx";
+import UpdateProduct from "./pages/UpdateProduct.page.tsx";
 
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   const [screenWidth, setScreenWidth] = useState(0);
@@ -26,6 +38,7 @@ function App() {
   //    setUser(res.jsonData);
   //  }
   //}
+  
 
   useEffect(() => {
   }, []);
@@ -45,14 +58,17 @@ function App() {
     <BrowserRouter>
       <Header screenWidth={screenWidth} />
       <main className="max-w-3xl mx-auto">
+        <ScrollToTop />
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/all_products" element={<AllProducts />} />
             <Route path="/single_fruit/:fruit_name/:productID" element={<SingleFruit />} />
             <Route path="/single_product/:productID" element={<SingleProduct />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/create_product" element={<CreateProduct />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/update_product" element={<UpdateProduct />} />
         </Routes>
       </main>
     </BrowserRouter>
