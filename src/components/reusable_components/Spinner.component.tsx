783b142ebda9453877f2be083d02f5bcbb0eed7b
gouravkotnala <gouravkotnala777@gmail.com>
2026-03-20 21:47:12 +0530

    feat(Spinner.component.tsx) : add a spinner component

---
+++ src/components/reusable_components/Spinner.component.tsx
@@ -0,0 +1,24 @@
+
+
+interface SpinnerPropTypes{
+    width?:string;
+    thickness?:string;
+    color?:string;
+}
+
+function Spinner({width="30px", thickness="4px", color="blue"}:SpinnerPropTypes) {
+    
+    return(
+        <div className="animate-spin border-2 border-red-500 w-30 h-30 rounded-full border-b-transparent"
+            style={{
+                width,
+                height:width,
+                border:`${thickness} solid ${color}`,
+                borderRight:`${thickness} solid transparent`
+            }}
+        >
+        </div>
+    )
+};
+
+export default Spinner;
